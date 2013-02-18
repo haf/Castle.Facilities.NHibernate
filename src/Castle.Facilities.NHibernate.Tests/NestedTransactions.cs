65afc817bd5fded0f2d97cc1422f9ea7b79d7d7e
kblok <dariokondratiuk@gmail.com>
2013-02-18 11:44:11 -0300

    A new NestedTransactions Test shows a desirable behavior

---
+++ src/Castle.Facilities.NHibernate.Tests/NestedTransactions.cs
@@ -0,0 +1,97 @@
+﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.NHibernate.Tests
+{
+	using System;
+	using System.Collections;
+	using System.Transactions;
+
+	using Castle.Facilities.AutoTx;
+	using Castle.Facilities.AutoTx.Testing;
+	using Castle.Facilities.NHibernate.Tests.Framework;
+	using Castle.Facilities.NHibernate.Tests.TestClasses;
+	using Castle.MicroKernel.Registration;
+	using Castle.Transactions;
+	using Castle.Windsor;
+
+	using NLog;
+
+	using NUnit.Framework;
+
+	using global::NHibernate;
+	using global::NHibernate.SqlCommand;
+	using global::NHibernate.Type;
+
+	using ITransaction = global::NHibernate.ITransaction;
+
+	internal class NestedTransactions : EnsureSchema
+	{
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+		private Container container;
+
+		[SetUp]
+		public void SetUp()
+		{
+			container = new Container();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			container.Dispose();
+		}
+
+		/// <summary>
+		/// This test shows that several transaction methods can be attached to the same transaction
+		/// </summary>
+		[Test]
+		public void RunTest()
+		{
+			logger.Debug("starting test run");
+
+			using (var x = container.ResolveScope<NestedTransactionService>())
+				x.Service.Run();
+		}
+	}
+
+	public class NestedTransactionService
+	{
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+		private readonly ISessionManager sessionManager;
+		private Guid thingId;
+
+		public NestedTransactionService(ISessionManager sessionManager)
+		{
+			this.sessionManager = sessionManager;
+		}
+
+		[Transaction]
+		public virtual void Run()
+		{
+
+			SaveNewThing();
+			SaveNewThing();
+		}
+
+		[Transaction]
+		protected virtual void SaveNewThing()
+		{
+			var s = sessionManager.OpenSession();
+			var thing = new Thing(18.0);
+			thingId = (Guid)s.Save(thing);
+		}
+
+	}
+}
--- src/Castle.Facilities.NHibernate.Tests/ValidationError_OnSave.cs
+++ src/Castle.Facilities.NHibernate.Tests/ValidationError_OnSave.cs
@@ -73,6 +73,7 @@ namespace Castle.Facilities.NHibernate.Tests
 			AddFacility<NHibernateFacility>();
 
 			Register(Component.For<Test>().LifeStyle.Transient);
+			Register(Component.For<NestedTransactionService>().LifeStyle.Transient);
 		}
 	}
 
@@ -81,7 +82,7 @@ namespace Castle.Facilities.NHibernate.Tests
 		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
 		public bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState,
-		                         string[] propertyNames, IType[] types)
+														 string[] propertyNames, IType[] types)
 		{
 			logger.Debug("throwing validation exception");
 
@@ -96,7 +97,7 @@ namespace Castle.Facilities.NHibernate.Tests
 		}
 
 		public int[] FindDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames,
-		                       IType[] types)
+													 IType[] types)
 		{
 			return null;
 		}
@@ -178,6 +179,7 @@ namespace Castle.Facilities.NHibernate.Tests
 		}
 	}
 
+
 	public class Test
 	{
 		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
@@ -189,6 +191,7 @@ namespace Castle.Facilities.NHibernate.Tests
 			this.sessionManager = sessionManager;
 		}
 
+		
 		public virtual void Run()
 		{
 			logger.Debug("run invoked");
@@ -232,4 +235,6 @@ namespace Castle.Facilities.NHibernate.Tests
 			return s.Load<Thing>(thingId);
 		}
 	}
+
+	
 }
